e4074336d3c006c606eb38a6df4771c536aada8e
ArnoldKermeci <k.arnold111@gmail.com>
2018-07-09 19:15:03 +0200

    Commit 2

--- app/src/main/java/com/example/androiddevelopment/testzavrsni/activity/ListActivity.java
+++ app/src/main/java/com/example/androiddevelopment/testzavrsni/activity/ListActivity.java
@@ -1,4 +1,114 @@
 package com.example.androiddevelopment.testzavrsni.activity;
 
-public class ListActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.androiddevelopment.testzavrsni.R;
+import com.example.androiddevelopment.testzavrsni.db.ORMLightHelper;
+import com.example.androiddevelopment.testzavrsni.db.model.Prijava;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+    private ORMLightHelper databaseHelper;
+    private SharedPreferences prefs;
+
+    public static String PRIJAVA_KEY = "PRIJAVA_KEY";
+    public static String NOTIF_TOAST = "notif_toast";
+    public static String NOTIF_STATUS = "notif_statis";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        final ListView listView = (ListView) findViewById(R.id.prijava_list);
+
+        try {
+            List<Prijava> list = getDatabaseHelper().getPrijavaDao().queryForAll();
+
+            ListAdapter adapter = new ArrayAdapter<>(ListActivity.this, R.layout.list_item, list);
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Prijava p = (Prijava) listView.getItemAtPosition(position);
+
+                    Intent intent = new Intent(ListActivity.this, DetailActivity.class);
+                    intent.putExtra(PRIJAVA_KEY, p.getmId());
+                    startActivity(intent);
+                }
+            });
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.list_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        refresh();
+    }
+
+    private void refresh() {
+        ListView listview = (ListView) findViewById(R.id.prijava_list);
+
+        if (listview != null){
+            ArrayAdapter<Prijava> adapter = (ArrayAdapter<Prijava>) listview.getAdapter();
+
+            if(adapter!= null)
+            {
+                try {
+                    adapter.clear();
+                    List<Prijava> list = getDatabaseHelper().getPrijavaDao().queryForAll();
+
+                    adapter.addAll(list);
+
+                    adapter.notifyDataSetChanged();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+/////////////////////
+        public ORMLightHelper getDatabaseHelper() {
+        if (databaseHelper == null) {
+            databaseHelper = OpenHelperManager.getHelper(this, ORMLightHelper.class);
+        }
+        return databaseHelper;
+    }
 }
--- app/src/main/java/com/example/androiddevelopment/testzavrsni/db/ORMLightHelper.java
+++ app/src/main/java/com/example/androiddevelopment/testzavrsni/db/ORMLightHelper.java
@@ -1,4 +1,72 @@
 package com.example.androiddevelopment.testzavrsni.db;
 
-public class ORMLightHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.androiddevelopment.testzavrsni.db.model.Prijava;
+import com.example.androiddevelopment.testzavrsni.db.model.Stavka;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
+public class ORMLightHelper extends OrmLiteSqliteOpenHelper {
+
+    private static final String DATABASE_NAME    = "db";
+    private static final int    DATABASE_VERSION = 1;
+
+    private Dao<Stavka, Integer> mStavkaDao = null;
+    private Dao<Prijava, Integer> mPrijavaDao = null;
+
+    public ORMLightHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, Stavka.class);
+            TableUtils.createTable(connectionSource, Prijava.class);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable(connectionSource, Stavka.class, true);
+            TableUtils.dropTable(connectionSource, Prijava.class, true);
+            onCreate(database, connectionSource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Dao<Stavka, Integer> getStavkaDao() throws SQLException {
+        if (mStavkaDao == null) {
+            mStavkaDao = getDao(Stavka.class);
+        }
+
+        return mStavkaDao;
+    }
+
+    public Dao<Prijava, Integer> getPrijavaDao() throws SQLException {
+        if (mPrijavaDao == null) {
+            mPrijavaDao = getDao(Prijava.class);
+        }
+
+        return mPrijavaDao;
+    }
+
+    @Override
+    public void close() {
+        mStavkaDao = null;
+        mPrijavaDao = null;
+
+        super.close();
+    }
 }
+
